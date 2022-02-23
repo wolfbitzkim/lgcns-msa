9a10050e4fae702c775eb8395335243416db6684
Dowon Lee <edowon0623@gmail.com>
2022-02-23 14:50:05 +0900

    LGE MSA 수업 3일차 (오후)

--- sources/msa-spring-cloud/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ sources/msa-spring-cloud/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -26,31 +26,31 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+//        http.authorizeRequests().antMatchers("/users/**").permitAll();
 
-//        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
-//        http.authorizeRequests().antMatchers("/health_check/**").permitAll();
-//        http.authorizeRequests().antMatchers("/login/**").permitAll();
-//        http.authorizeRequests().antMatchers("/**")
-////                .hasIpAddress(env.getProperty("gateway.ip")) // <- IP 변경
-//                .access("hasIpAddress('192.168.0.38') or hasIpAddress('127.0.0.1/32')")
-//                .and()
-//                .addFilter(getAuthenticationFilter());
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
+        http.authorizeRequests().antMatchers("/health_check/**").permitAll();
+        http.authorizeRequests().antMatchers("/login/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+//                .hasIpAddress(env.getProperty("gateway.ip")) // <- IP 변경
+                .access("hasIpAddress('192.168.0.10') or hasIpAddress('127.0.0.1/32')")
+                .and()
+                .addFilter(getAuthenticationFilter());
 
         http.headers().frameOptions().disable();
     }
 
-//    private AuthenticationFilter getAuthenticationFilter() throws Exception {
-//        AuthenticationFilter authenticationFilter =
-//                new AuthenticationFilter(authenticationManager(), userService, env);
-//
-//        return authenticationFilter;
-//    }
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter =
+                new AuthenticationFilter(authenticationManager(), userService, env);
+
+        return authenticationFilter;
+    }
 
     // select pwd from users where email=?
     // db_pwd(encrypted) == input_pwd(encrypted)
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
-//    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+    }
 }
